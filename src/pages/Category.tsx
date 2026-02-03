34bfdff62a78f8e680e2c1f5bbbd46c3dc43261f
faldubhoomi4 <faldubhoomi4@gmail.com>
2026-02-03 14:37:46 +0530

    add router

---
+++ src/pages/Category.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Category() {
+  return (
+    <div>Category</div>
+  )
+}
+
+
---
+++ src/router.tsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  { path: "/", element: <h1>Home Page</h1> },   
+   { path: "/Category", element: <h1>Category Page</h1> },   
+]);
+    
